01109ccf11aa9678dbc1fbeda25e4dc8bce600d3
Tabosa <31171088+Tabosa@users.noreply.github.com>
2023-10-22 20:14:02 -0300

    Adicionar atualizações de configuração do projeto

--- Forestry_Blockchain/Forestry_Blockchain/asgi.py
+++ Forestry_Blockchain/Forestry_Blockchain/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ForetryBlockchain project.
+ASGI config for Forestry_Blockchain project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ForetryBlockchain.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Forestry_Blockchain.settings')
 
 application = get_asgi_application()
--- Forestry_Blockchain/Forestry_Blockchain/wsgi.py
+++ Forestry_Blockchain/Forestry_Blockchain/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ForetryBlockchain project.
+WSGI config for Forestry_Blockchain project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ForetryBlockchain.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Forestry_Blockchain.settings')
 
 application = get_wsgi_application()
